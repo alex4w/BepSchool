3612040ecd906f579e4f365f52b045dc21cdcabd
alexcarranza4w <git@gitlab.com>
2018-11-04 22:21:44 -0500

    First commit

---
+++ mysql_login.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Provee las constantes para conectarse a la base de datos
+ * Mysql.
+ */
+define("HOSTNAME", "localhost");// Nombre del host
+define("DATABASE", "beptechn_school"); // Nombre de la base de datos  define
+define("USERNAME", "beptechn_ambatoservices_opmobile"); // Nombre del usuario
+define("PASSWORD", "Opmobile.2018."); // Nombre de la constrase?a define("PASSWORD", "");
+?>
