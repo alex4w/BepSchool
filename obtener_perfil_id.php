87cc2b1bfc8584318d48f4d936ff4d18d8055208
alexcarranza4w <git@gitlab.com>
2018-11-05 17:46:24 -0500

    sss

---
+++ obtener_perfil_id.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Obtiene todas las metas de la base de datos
+ */
+
+require 'School.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+	$id = $_GET['id'];
+	
+    // Manejar petición GET
+    $perfil = Meta::getPerfilById($id);
+	
+	
+
+    if ($perfil) {
+		$imagen = Meta::getFotoById($id);
+		$perfil["imagen"] = $imagen;
+        $datos["estado"] = 1;
+		$datos["perfil"] = $perfil;
+		
+
+        print json_encode($datos);
+
+
+    } else {
+        print json_encode(array(
+            "estado" => 2,
+            "mensaje" => "Ha ocurrido un error"
+        ));
+    }
+}
+
